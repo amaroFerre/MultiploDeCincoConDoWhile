c76e49deb689e04429c43c3067a3f89bea00276f
Amarico <Amarico@equipo-zalman>
2018-09-10 19:04:51 +0200

    Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle do-while.

---
+++ src/multiplosdecincocondowhile/MultiplosDeCincoConDoWhile.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package multiplosdecincocondowhile;
+
+/**
+ *Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle do-while.
+ * @author Amarico
+ */
+public class MultiplosDeCincoConDoWhile {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    // TODO code application logic here
+  }
+  
+}
